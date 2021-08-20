8ab6f4e27abbc267b935f0d33d11470e4d1d33cd
ducky <luckyduck0734@gmail.com>
2021-08-20 09:23:35 -0500

    making embed for sticker info command

--- slash/info.js
+++ slash/info.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     async execute(interaction, data) {
         const input = data.query;
@@ -8,7 +10,26 @@ module.exports = {
             sticker = interaction.guild.stickers.cache.find(sticker => sticker.name == input || sticker.id == input);
         };
         if (sticker) {
-            interaction.reply(JSON.stringify(sticker));
+            if (sticker.format !== 'LOTTIE') {
+                sticker.link = `https://cdn.discordapp.com/stickers/${sticker.id}.png`;
+            };
+            const embed = new MessageEmbed()
+                .setTitle(sticker.name)
+                .setFooter('Sticker ID: ' + sticker.id)
+            if (sticker.description) embed.setDescription(sticker.description);
+            if (sticker?.link) {
+                embed.setThumbnail(sticker.link);
+            } else {
+                embed.description = 'Unable to display sticker!\n' + embed.description;
+            };
+            if (sticker.tags) {
+                let tags = '';
+                sticker.tags.forEach(tag => {
+                    tags += `:${tag}: `
+                });
+                embed.addField('Tags:', tags, true);
+            };
+            interaction.reply({ embeds: [embed] });
         } else {
             interaction.reply('No sticker found :(');
         };
